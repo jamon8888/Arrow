e18598d4cb7fe008db13300d126a559d2294c400
Daniel Saxil-Nielsen <daniel@saxil-nielsen.com>
2013-02-15 08:48:00 +0000

    pie chart should no longer load up with false

--- public/scripts/models/datasource/DataSourceModel.js
+++ public/scripts/models/datasource/DataSourceModel.js
@@ -149,6 +149,7 @@ define([
 
 			// if the key is in the black list, shift an item off
 			if (_.indexOf(this.blacklist, key) !== -1) {
+				console.log(key + ' is in the blacklist so not storing');
 				return;
 			}
 
