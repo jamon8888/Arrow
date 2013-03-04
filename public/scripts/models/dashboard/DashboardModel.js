efe3ce3d57d2495f16c8906b947d05023c29d2bb
Daniel Saxil-Nielsen <daniel@saxil-nielsen.com>
2013-03-04 12:07:00 +0000

    dashboards get their own colors

--- public/scripts/models/dashboard/DashboardModel.js
+++ public/scripts/models/dashboard/DashboardModel.js
@@ -14,7 +14,7 @@ define([
 			title: '',
 			hidden: true,
 			visualizations: new VisualizationCollection(),
-			allColors: COLOR,
+			allColors: ['#f04747', '#00afd4', '#ebaf3c', '#38b87c', '#e9662c'],
 			color: '#e9e9e9',
 			startTime: false,
 			endTime: false
