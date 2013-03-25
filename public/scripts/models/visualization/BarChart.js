2f361c254b297d8ef401d56d034ed2624fd4aee1
Daniel Saxil-Nielsen <daniel@saxil-nielsen.com>
2013-03-25 12:11:47 +0000

    Changed the color scheme

--- public/scripts/models/visualization/BarChart.js
+++ public/scripts/models/visualization/BarChart.js
@@ -51,7 +51,9 @@ define([
 				.attr('y', function (d) { return range(d.value); })
 				.attr('height', function (d) { return height - range(d.value) ; })
 				.attr('width', function (d) { return (showy ? (width - yWidth) : width) / this.data.length; }.bind(this))
-				.attr('fill', function (d, i) { return this.colors(i); }.bind(this));
+				.attr('fill', function (d, i) { return this.colors(i); }.bind(this))
+				.on('mouseover', _.bind(this.mouseOver, this))
+				.on('mouseout', _.bind(this.mouseOut, this));
 
 			if (showy) {
 				var yaxis = chart.append('g')
@@ -60,7 +62,7 @@ define([
 					.call(d3.svg.axis().scale(range).orient('left').ticks(5));
 			}
 
-			if (showx) {
+			if (showx && (showy ? (width - yWidth) : width) / this.data.length > 20) {
 				var xaxis = chart.append('g')
 					.attr('class', 'xaxis')
 					.attr('transform', 'translate(' + 0 + ',' + (height - 20) + ')');
@@ -76,6 +78,8 @@ define([
 					.text(function (d) {
 						return d.label;
 					});
+			} else {
+
 			}
 		},
 
@@ -93,6 +97,38 @@ define([
 
 			this.draw();
 			return this.div;
+		},
+
+		/**
+		 * When we hover over the pie, show a tooltip with the current key we are
+		 * hovering
+		 * 
+		 * @param  {Object} evt Mouse hover event
+		 */
+		mouseOver: function (evt) {
+
+			this.tooltip = document.createElement('div');
+			this.tooltip.id = 'tooltip';
+			this.tooltip.innerHTML = evt.label;
+
+			$(this.tooltip).css({
+				'left': (window.pageXOffset + d3.event.clientX) + 'px',
+				'top': (window.pageYOffset + d3.event.clientY) + 'px'
+			});
+
+			document.body.appendChild(this.tooltip);
+		},
+
+		/**
+		 * Just remove the tooltip when we mouse out
+		 *
+		 * @todo This could be down a lot better, currently this will flickr if you hover
+		 * the tooltip
+		 * 
+		 * @param  {Object} evt mouse out event
+		 */
+		mouseOut: function (evt) {
+			$(this.tooltip).remove();
 		},
 
 		process: function (data, group) {
